c12e2862c6448c5451e0df85ebc77f676d0cd5f8
vethumIIT <vethum.20231974@iit.ac.lk>
2024-12-03 22:04:10 +0530

    Initialized Spring Boot Project
    Initialized the spring boot project for the backend.

---
+++ BACKEND/Fably-Fashion-App/src/main/java/com/project/Fably_Fashion_App/FablyFashionAppApplication.java
@@ -0,0 +1,13 @@
+package com.project.Fably_Fashion_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FablyFashionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FablyFashionAppApplication.class, args);
+	}
+
+}
---
+++ BACKEND/Fably-Fashion-App/src/test/java/com/project/Fably_Fashion_App/FablyFashionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Fably_Fashion_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FablyFashionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
